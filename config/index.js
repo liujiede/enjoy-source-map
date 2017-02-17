6b8fac15970c2720b911f5775ddbb86b5d3bbd18
刘杰 <jie.liu5@corp.elong.com>
2017-02-17 12:57:08 +0800

    add config sm

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
+    SM: false,
     T_MATCHS: ['content', 'mods'],
     INLINE_SM_REG: /^\s*\/(?:\/|\*)[@#]\s+sourceMappingURL=data:(?:application|text)\/json;(?:charset[:=]\S+;)?base64,.*?$/m
 }

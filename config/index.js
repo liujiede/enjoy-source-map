298c380134baa53389135d1e7799ccb24e27ae4d
刘杰 <liujie48721@163.com>
2017-03-13 18:54:02 +0800

    修复bug(编译rn framework 报无效source-map)、关闭配置文件机制，改为判断内容、修改合并逻辑，没有sourcemap的追加合并

--- config/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    SM: false
-}
--- test/convert.js
+++ test/convert.js
@@ -91,7 +91,7 @@ function run() {
     console.log('args:', process.argv.join());
 
     testVlq();
-    setEnvByArgv('--sm'); //fromBuild
+    setEnvByArgv('--sm'); //generate source-map
     setEnvByArgv('--fb'); //fromBuild
     if (process.env.FB) {
         setEnvByArgv('--nw') //noWrite
